154e568c162d58468b74ebd6d1701244afd3fb2d
Eric <ericpeter.f@gmail.com>
2022-09-19 17:05:32 -0400

    BE working?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 
-const App = () => {
 
+const App = () => {
   const [deck, setDeck] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
+    fetch("/api/flashcards/")
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  });
 
-    fetch('/api/')
-
-  } , [])
-
-  return (<h1>Redo</h1>)
-}
+  return <h1>Redo</h1>;
+};
 
 export default App;
