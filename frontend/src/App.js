221399e38d615ba959fa8d4e0a3ef5cad8bb6153
Eric <ericpeter.f@gmail.com>
2022-09-19 22:59:22 -0400

    draw

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 
 const App = () => {
   const [deck, setDeck] = useState([]);
+  const [drawOrder, setDrawOrder] = useState([])
 
   useEffect(() => {
     fetch("/api/flashcards/")
-      .then((res) => console.log(res))
+      .then((res) => (res.json())).then((data) => {
+        setDeck(data);        
+      })
       .catch((err) => console.log(err));
   });
+
+  useEffect(() => {
+    if(! drawOrder.length){
+      setDrawOrder( deck.map(card => card.id))
+    }
+  }, [deck])
+
 
   return <h1>Redo</h1>;
 };
